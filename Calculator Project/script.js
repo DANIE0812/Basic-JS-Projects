96c542f52f0516f4f7419d6d33c80e28403b0bbd
DANIE0812 <45457280+DANIE0812@users.noreply.github.com>
2020-04-15 16:38:36 +0100

    initial commit

---
+++ Calculator Project/script.js
@@ -0,0 +1,27 @@
+const btn = document.querySelectorAll('.btn');
+const screen = document.querySelector('.screen');
+const equalBtn = document.querySelector('.btn-equal');
+const clearBtn = document.querySelector('.btn-clear');
+
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function(){
+        let number = btn[i].getAttribute('data-num');
+        screen.value += number 
+    })
+}
+
+equalBtn.addEventListener('click', function(){
+    if (screen.value === ''){
+        alert('Input is empty')
+    } 
+    else {
+        let value = eval(screen.value)
+        screen.value = value
+    }
+})
+
+clearBtn.addEventListener('click', function (){
+    screen.value = '';
+})
+
