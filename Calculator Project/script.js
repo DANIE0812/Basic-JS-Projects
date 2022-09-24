338f8ebb85aaf15fb2467e4ac692550790e968c4
DANIE0812 <45457280+DANIE0812@users.noreply.github.com>
2022-09-24 20:12:03 +0100

    updated js ES5 to ES6

--- Calculator Project/script.js
+++ Calculator Project/script.js
@@ -1,3 +1,4 @@
+
 const btn = document.querySelectorAll('.btn');
 const screen = document.querySelector('.screen');
 const equalBtn = document.querySelector('.btn-equal');
@@ -5,13 +6,13 @@ const clearBtn = document.querySelector('.btn-clear');
 
 
 for (let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener('click', function(){
+    btn[i].addEventListener('click', () => {
         let number = btn[i].getAttribute('data-num');
         screen.value += number 
-    })
+    });
 }
 
-equalBtn.addEventListener('click', function(){
+equalBtn.addEventListener('click', () => {
     if (screen.value === ''){
         alert('Input is empty')
     } 
@@ -19,9 +20,8 @@ equalBtn.addEventListener('click', function(){
         let value = eval(screen.value)
         screen.value = value
     }
-})
+});
 
-clearBtn.addEventListener('click', function (){
+clearBtn.addEventListener('click', () => {
     screen.value = '';
-})
-
+});
